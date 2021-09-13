9f489bb0e4023071f9b0c1343a87f8f31b807074
noamgolani <noam.golani@gmail.com>
2021-09-13 17:24:47 +0300

    verry basic playing indication

--- scripts/elements/playerElement.js
+++ scripts/elements/playerElement.js
@@ -1,9 +1,11 @@
 import { createElement } from "../helpers.js";
 
 export default class playerElement {
-   constructor(song, top, left) {
+   constructor(song, top = 0, left = 0) {
       this.song = song;
       this.element = this.createPlayerElement(song);
+      this.element.style.top = top;
+      this.element.style.left = left;
    }
 
    createPlayerElement({ title, coverArt }) {
