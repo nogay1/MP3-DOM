74e627738c3cafa9e7cdac14387c7c1e5fbcd82f
noamgolani <noam.golani@gmail.com>
2021-09-14 12:30:21 +0300

    progress bar moves

--- scripts/elements/playerElement.js
+++ scripts/elements/playerElement.js
@@ -1,30 +1,30 @@
-import { createElement } from "../helpers.js";
+import { createElement, formatDuration } from "../helpers.js";
 
 export default class playerElement {
-   constructor(song, parentElement, top = 0, left = 0) {
-      this._song = song;
-      this._top = top;
-      this._left = left;
-      this._parentElement = parentElement;
-      this._element = this.createPlayerElement(song);
-      this._parentElement.append(this._element);
+   constructor(playerElement, currentSong) {
+      this._playerElement = playerElement;
+      this._passedTime = 0;
+      this.song = currentSong;
+      this._countingIntervel = setInterval(() => {
+         this.tick();
+      }, 1000);
    }
 
    set song(song) {
       this._song = song;
-      this._parentElement.removeChild(this._element);
-      this._element = this.createPlayerElement(this._song);
-      this._parentElement.append(this._element);
+      const { coverArt, duration } = song;
+
+      const $ = (sel) => this._playerElement.querySelector(sel);
+
+      $(".info img").setAttribute("src", coverArt);
+      $("#time").innerText = formatDuration(duration);
    }
 
-   createPlayerElement({ title, coverArt }) {
-      const children = [
-         createElement("img", [], [], { src: coverArt }),
-         createElement("span", title),
-      ];
-      const element = createElement("div", children, "player");
-      element.style.top = this._top;
-      element.style.left = this._left;
-      return element;
+   tick() {
+      this._passedTime++;
+      const $ = (sel) => this._playerElement.querySelector(sel);
+      $("#progressPoint").style.left =
+         Math.floor((100 * this._passedTime) / this._song.duration) + "%";
+      $("#negativeTime").innerText = formatDuration(this._passedTime);
    }
 }
